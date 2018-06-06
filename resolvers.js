b1d77b2c279d7a6d8caea3a4b20dc17096d6c0ec
Paola Ortiz de Zevallos Villanueva <paolaozv@gmail.com>
2018-06-06 18:41:35 -0500

    Method Add profesor register

--- resolvers.js
+++ resolvers.js
@@ -9,6 +9,9 @@ const resolvers = {
     profesores: () => Profesor.query().eager('cursos'),
     curso: (rootValue, args) => Curso.query().eager('[profesor, comentarios]').findById(args.id),
     profesor: (rootValue, args) => Profesor.query().eager('cursos').findById(args.id)
+  },
+  Mutation: {
+    profesorAdd: (_, args) => Profesor.query().insert(args.profesor)
   }
 }
 
