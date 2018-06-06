fa89285bfc29d9eac379bc5d53dadfc04ab4652f
Paola Ortiz de Zevallos Villanueva <paolaozv@gmail.com>
2018-06-06 12:31:39 -0500

    Add creation schema

--- schema.js
+++ schema.js
@@ -1,18 +1,43 @@
 const { makeExecutableSchema } = require('graphql-tools');
 
 const typeDefs = `
+  # Esto es un curso en el sistema
   type Curso {
     id: ID!
     titulo: String!
+    # Esta es la descripcion del curso
+    descripcion: String!
+    rating: Float
+    comentarios: [Comentario]
+  }
+
+  type Profesor {
+    id: ID!
+    nombre: String!
+    nacionalidad: String!
+    genero: Genero
+    cursos: [Curso]
+  }
+
+  enum Genero {
+    MASCULINO
+    FEMENINO
+  }
+
+  type Comentario {
+    id: ID!
+    nombre: String!
+    cuerpo: String!
   }
 
   type Query {
     cursos: [Curso]
+    profesores: [Profesor]
+    curso(id: Int): Curso
+    profesor(id: Int): Profesor
   }
 `;
 
-const schema = makeExecutableSchema({
-  typeDefs
-});
+const schema = makeExecutableSchema({ typeDefs });
 
 module.exports = schema;
